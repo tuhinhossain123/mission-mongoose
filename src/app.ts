8a74393c46e2c70bd25761bca05d6ce530278166
tuhinhossain123 <csetuhin55@gmail.com>
2025-03-18 15:29:19 +0600

    create index route and code clean

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student-route';
-import { UserRoutes } from './app/modules/user/user-route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 
 // parser
@@ -11,15 +10,14 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-const getController = (req: Request, res: Response) => {
+const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get('/', getController);
+app.get('/', test);
 
 //global error handler
 app.use(globalErrorHandler);
