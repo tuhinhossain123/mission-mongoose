bedcf193247b9818c836c53073389d23238913e8
tuhinhossain123 <csetuhin55@gmail.com>
2025-03-19 21:43:22 +0600

    academic semester schema and model created

--- src/app/modules/academic-semester/academic-semester-interface.ts
+++ src/app/modules/academic-semester/academic-semester-interface.ts
@@ -1,4 +1,4 @@
-export type TMonth =
+export type TMonths =
   | 'January'
   | 'February'
   | 'March'
@@ -16,6 +16,6 @@ export type TAcademicSemester = {
   name: 'Autum' | 'Summar' | 'Fall';
   code: '01' | '02' | '03';
   year: Date;
-  startMonth: TMonth;
-  endMonth: TMonth;
+  startMonth: TMonths;
+  endMonth: TMonths;
 };
---
+++ src/app/modules/academic-semester/academic-semester-model.ts
@@ -0,0 +1,43 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicSemester, TMonths } from './academic-semester-interface';
+
+const months: TMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: { type: String, required: true },
+    year: { type: Date, required: true },
+    code: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      enum: months,
+    },
+    endMonth: {
+      type: String,
+      enum: months,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemester = model<TAcademicSemester>(
+  'academicSemester',
+  academicSemesterSchema,
+);
