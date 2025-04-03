d25769464d67d75100726e3ef47d64b62c65c7f8
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-03 18:04:58 +0600

    academic semester created successfully

--- src/app/modules/user/user-utils.ts
+++ src/app/modules/user/user-utils.ts
@@ -13,13 +13,25 @@ const findLastStudent = async () => {
   )
     .sort({ createAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // year semester code 4 digit number
 export const generetdStudentId = async (payload: TAcademicSemester) => {
   //first time 0000
-  const currentId = (await findLastStudent()) || (0).toString();
+  let currentId = (0).toString(); //0000 by default
+  const lastStudent = await findLastStudent();
+  const lastStudentSemesterCode = lastStudent?.substring(4, 6);
+  const lastStudentYear = lastStudent?.substring(0, 4);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+  if (
+    lastStudent &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudent.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
