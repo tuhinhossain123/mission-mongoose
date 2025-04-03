10912a499712441c579b0b3b9bf6555a3ee8447a
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-03 22:20:15 +0600

    academic department routes added

---
+++ src/app/modules/academic-department/academic-department-route.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicDepartmentValidation } from './academic-department-validation';
+import { AcademicDepartmentControllers } from './academic-department-controller';
+
+const router = Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    academicDepartmentValidation.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartment,
+);
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+
+router.get(
+  '/:departmentId',
+  AcademicDepartmentControllers.getSingleAcademicDepartment,
+);
+
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    academicDepartmentValidation.updateAcademicDepartValidationSchema,
+  ),
+  AcademicDepartmentControllers.updaAcademicDepartment,
+);
+
+export const AcademicDepartmentRoute = router;
