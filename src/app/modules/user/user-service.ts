9c39452726a2085ba1ce84a1476cce8c28dab308
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-04 10:30:11 +0600

    Implement transaction & rollback

--- src/app/modules/user/user-service.ts
+++ src/app/modules/user/user-service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import config from '../../config';
 import { AcademicSemesterModel } from '../academic-semester/academic-semester-model';
 import { TStudent } from '../student/student-interface';
@@ -5,7 +6,8 @@ import { Student } from '../student/student-model';
 import { TUser } from './user-interface';
 import { User } from './user-model';
 import { generetdStudentId } from './user-utils';
-
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
 // post request
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
   //------------for creating an static method----------- student service theke asce code eta
@@ -27,29 +29,34 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
     payload.admissionSemester,
   );
 
-  //   set manually generated id
-  userData.id = await generetdStudentId(admisionSemester);
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    //   set manually generated id
+    userData.id = await generetdStudentId(admisionSemester);
 
-  //------- create a user
-  const NewUser = await User.create(userData); //built in static method
+    //------- create a user (transtion -1)
+    const NewUser = await User.create([userData], { session }); //built in static method
 
-  //   ------ create a student
-  if (Object.keys(NewUser).length) {
+    //   ------ create a student
+    if (!NewUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
     //set id,_id as user
-    payload.id = NewUser.id;
-    payload.user = NewUser._id; //reference _id
-
-    const newStudent = await Student.create(payload);
+    payload.id = NewUser[0].id;
+    payload.user = NewUser[0]._id; //reference _id
+    // create a student (transtion -2)
+    const newStudent = await Student.create([payload], { session });
+    if (!newStudent.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
+    }
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession;
   }
-  return NewUser;
-  //---------------for creating an custom instance method--------------------------- student service theke asce code eta
-  //   const student = new Student(studentData); // create an instance
-  //   if (await student.isUserExists(studentData.id)) {
-  //     throw new Error('User already exists');
-  //   }
-  //   const result = await student.save(); //bult in instance method
-  //   return result;
 };
 
 export const UserService = {
