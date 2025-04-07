9578a59921a2d2bb06032c017d96a690ff7a3dfc
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-07 10:18:00 +0600

    code refactor

--- src/app/modules/user/user-service.ts
+++ src/app/modules/user/user-service.ts
@@ -1,60 +1,183 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import httpStatus from 'http-status';
 import mongoose from 'mongoose';
 import config from '../../config';
-import { AcademicSemesterModel } from '../academic-semester/academic-semester-model';
+import AppError from '../../errors/AppError';
+import { Admin } from '../Admin/admin-model';
+import { TFaculty } from '../Faculty/faculty-interface';
+import { Faculty } from '../Faculty/faculty-model';
+import { AcademicDepartment } from '../academic-department/academic-department-model';
 import { TStudent } from '../student/student-interface';
 import { Student } from '../student/student-model';
+import { AcademicSemesterModel } from '../academic-semester/academic-semester-model';
 import { TUser } from './user-interface';
 import { User } from './user-model';
-import { generetdStudentId } from './user-utils';
-import AppError from '../../errors/AppError';
-import httpStatus from 'http-status';
-// post request
+import {
+  generateAdminId,
+  generateFacultyId,
+  generateStudentId,
+} from './user-utils';
+
 const createStudentIntoDB = async (password: string, payload: TStudent) => {
-  //   ------ creat a user object
+  // create a user object
   const userData: Partial<TUser> = {};
 
-  //  -------- if password is not given , use default password------
+  //if password is not given , use deafult password
   userData.password = password || (config.default_pass as string);
 
-  //   -------set student role-------
+  //set student role
   userData.role = 'student';
 
   // find academic semester info
-  const admisionSemester = await AcademicSemesterModel.findById(
+  const admissionSemester = await AcademicSemesterModel.findById(
     payload.admissionSemester,
   );
 
+  if (!admissionSemester) {
+    throw new AppError(400, 'Admission semester not found');
+  }
+
   const session = await mongoose.startSession();
+
   try {
     session.startTransaction();
-    //   set manually generated id
-    userData.id = await generetdStudentId(admisionSemester);
+    //set  generated id
+    userData.id = await generateStudentId(admissionSemester);
 
-    //------- create a user (transtion -1)
-    const NewUser = await User.create([userData], { session }); //built in static method
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session }); // array
 
-    //   ------ create a student
-    if (!NewUser.length) {
+    //create a student
+    if (!newUser.length) {
       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
     }
-    //set id,_id as user
-    payload.id = NewUser[0].id;
-    payload.user = NewUser[0]._id; //reference _id
-    // create a student (transtion -2)
+    // set id , _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
+
+    // create a student (transaction-2)
+
     const newStudent = await Student.create([payload], { session });
+
     if (!newStudent.length) {
       throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create student');
     }
+
     await session.commitTransaction();
     await session.endSession();
+
     return newStudent;
-  } catch (err) {
+  } catch (err: any) {
     await session.abortTransaction();
-    await session.endSession;
+    await session.endSession();
     throw new Error(err);
   }
 };
 
-export const UserService = {
+const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
+  // create a user object
+  const userData: Partial<TUser> = {};
+
+  //if password is not given , use deafult password
+  userData.password = password || (config.default_pass as string);
+
+  //set student role
+  userData.role = 'faculty';
+
+  // find academic department info
+  const academicDepartment = await AcademicDepartment.findById(
+    payload.academicDepartment,
+  );
+
+  if (!academicDepartment) {
+    throw new AppError(400, 'Academic department not found');
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //set  generated id
+    userData.id = await generateFacultyId();
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session }); // array
+
+    //create a faculty
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+    // set id , _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
+
+    // create a faculty (transaction-2)
+
+    const newFaculty = await Faculty.create([payload], { session });
+
+    if (!newFaculty.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create faculty');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newFaculty;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+};
+
+const createAdminIntoDB = async (password: string, payload: TFaculty) => {
+  // create a user object
+  const userData: Partial<TUser> = {};
+
+  //if password is not given , use deafult password
+  userData.password = password || (config.default_pass as string);
+
+  //set student role
+  userData.role = 'admin';
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //set  generated id
+    userData.id = await generateAdminId();
+
+    // create a user (transaction-1)
+    const newUser = await User.create([userData], { session });
+
+    //create a admin
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    }
+    // set id , _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; //reference _id
+
+    // create a admin (transaction-2)
+    const newAdmin = await Admin.create([payload], { session });
+
+    if (!newAdmin.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return newAdmin;
+  } catch (err: any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
+};
+
+export const UserServices = {
   createStudentIntoDB,
+  createFacultyIntoDB,
+  createAdminIntoDB,
 };
--- src/app/modules/user/user-utils.ts
+++ src/app/modules/user/user-utils.ts
@@ -1,7 +1,9 @@
+
 import { TAcademicSemester } from '../academic-semester/academic-semester-interface';
 import { User } from './user-model';
 
-const findLastStudent = async () => {
+
+const findLastStudentId = async () => {
   const lastStudent = await User.findOne(
     {
       role: 'student',
@@ -11,28 +13,102 @@ const findLastStudent = async () => {
       _id: 0,
     },
   )
-    .sort({ createAt: -1 })
+    .sort({
+      createdAt: -1,
+    })
     .lean();
+
   return lastStudent?.id ? lastStudent.id : undefined;
 };
 
-// year semester code 4 digit number
-export const generetdStudentId = async (payload: TAcademicSemester) => {
-  //first time 0000
-  let currentId = (0).toString(); //0000 by default
-  const lastStudent = await findLastStudent();
-  const lastStudentSemesterCode = lastStudent?.substring(4, 6);
-  const lastStudentYear = lastStudent?.substring(0, 4);
+export const generateStudentId = async (payload: TAcademicSemester) => {
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+
   const currentSemesterCode = payload.code;
   const currentYear = payload.year;
+
   if (
-    lastStudent &&
+    lastStudentId &&
     lastStudentSemesterCode === currentSemesterCode &&
     lastStudentYear === currentYear
   ) {
-    currentId = lastStudent.substring(6);
+    currentId = lastStudentId.substring(6);
   }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
   incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
+};
+
+// Faculty ID
+export const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne(
+    {
+      role: 'faculty',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyId();
+
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `F-${incrementId}`;
+
+  return incrementId;
+};
+
+// Admin ID
+export const findLastAdminId = async () => {
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
+
+export const generateAdminId = async () => {
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
+
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `A-${incrementId}`;
   return incrementId;
 };
