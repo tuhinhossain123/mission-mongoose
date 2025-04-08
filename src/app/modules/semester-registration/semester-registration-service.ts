00076d7cf8e52a121d5e0ef995d2eea6de8c6de7
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-08 22:34:07 +0600

    Apply Business Logic for creating and updating semester registration

--- src/app/modules/semester-registration/semester-registration-service.ts
+++ src/app/modules/semester-registration/semester-registration-service.ts
@@ -10,6 +10,18 @@ const createSemesterregistrationIntoDB = async (
 ) => {
   const academicSemester = payload?.academicSemester;
 
+  // check if the any registered semester that is already 'UPCOMING'|"ONGOING"
+  const isThereAnyUpComingOrOngoingSemester =
+    await SemesterRegistration.findOne({
+      $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
+    });
+  if (isThereAnyUpComingOrOngoingSemester) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `There is already a ${isThereAnyUpComingOrOngoingSemester.status} register semester !!`,
+    );
+  }
+
   //   check if the semester is exist
   const isAcademicSemesterExist =
     await AcademicSemester.findById(academicSemester);
@@ -38,14 +50,17 @@ const createSemesterregistrationIntoDB = async (
 const getAllSemesterRegistrationIntoDB = async (
   query: Record<string, unknown>,
 ) => {
-  const adminQuery = new QueryBuilder(SemesterRegistration.find().populate('academicSemester'), query)
-  .filter()
-  .sort()
-  .paginate()
-  .fields();
+  const adminQuery = new QueryBuilder(
+    SemesterRegistration.find().populate('academicSemester'),
+    query,
+  )
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
 
-const result = await adminQuery.modelQuery;
-return result;
+  const result = await adminQuery.modelQuery;
+  return result;
 };
 
 const getSingleSemesterRegistrationFromDB = async (id: string) => {
@@ -57,14 +72,21 @@ const updateSemesterRegistrationIntoDB = async (
   id: string,
   payload: Partial<TSemesterRegistration>,
 ) => {
-  const result = await SemesterRegistration.findOneAndUpdate(
-    { _id: id },
-    payload,
-    {
-      new: true,
-    },
-  );
-  return result;
+  //check if the requested registerd semester is exists
+  const isSemesterRegistrationExists = await SemesterRegistration.findById(id);
+
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This semester is no found');
+  }
+
+  // if the requested semester redisration is ended, we will not update anything
+  const currenstSemesterStatus = isSemesterRegistrationExists?.status;
+  if (currenstSemesterStatus === 'ENDED') {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This semester is alredy ${currenstSemesterStatus}`,
+    );
+  }
 };
 
 export const SemesterRegistrationServices = {
