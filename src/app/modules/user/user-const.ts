dc887f08d34aa6a6fcadd2afb6be858390f949de
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-11 21:22:47 +0600

    Implement authorization using auth

---
+++ src/app/modules/user/user-const.ts
@@ -0,0 +1,7 @@
+export const userRole = {
+  student: 'student',
+  faculty: 'faculty',
+  admin: 'admin',
+} as const;
+
+
--- src/app/modules/user/user-route.ts
+++ src/app/modules/user/user-route.ts
@@ -3,15 +3,19 @@ import { UserControllers } from './user-controller';
 import { studentValidations } from '../student/student-validation-zod';
 import validateRequest from '../../middlewares/validateRequest';
 import { AdminValidations } from '../Admin/admin-validation';
+import auth from '../../middlewares/auth';
+import { userRole } from './user-const';
 const router = express.Router();
 
 router.post(
   '/create-student',
+  auth(userRole.admin),
   validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
 router.post(
   '/create-faculty',
+  auth(userRole.admin),
   // validateRequest(studentValidations.createStudentValidationSchema),
   UserControllers.createFaculty,
 );
