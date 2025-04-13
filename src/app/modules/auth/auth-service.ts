3c0499d5e5190feb4585f0dd62ac6dd03b0d5a0e
tuhinhossain123 <csetuhin55@gmail.com>
2025-04-13 21:36:59 +0600

    Reset old password , hash it _ saved into DB

--- src/app/modules/auth/auth-service.ts
+++ src/app/modules/auth/auth-service.ts
@@ -181,7 +181,7 @@ const forgatePassword = async (userId: string) => {
 
 const resetPassword = async (
   payload: { id: string; newPassword: string },
-  token,
+  token: string,
 ) => {
   // checking if  the user is exist
   const user = await User.isUserExistsByCustomId(payload?.id);
@@ -199,6 +199,31 @@ const resetPassword = async (
   if (userStatus === 'blocked') {
     throw new AppError(httpStatus.FORBIDDEN, 'This user is already blocked!!');
   }
+  const decoded = jwt.verify(
+    token,
+    config.jwt_access_secret as string,
+  ) as JwtPayload;
+
+  if (payload.id !== decoded.userId) {
+    throw new AppError(httpStatus.FORBIDDEN, 'You are Forbiden');
+  }
+
+  // hash new password
+  const newHashedPassword = await bcrypt.hash(
+    payload.newPassword,
+    Number(config.bcrypt_salt_rounds),
+  );
+  await User.findOneAndUpdate(
+    {
+      id: decoded?.userId,
+      role: decoded.role,
+    },
+    {
+      password: newHashedPassword,
+      needsPasswordChange: false,
+      passwordChangedAt: new Date(),
+    },
+  );
 };
 
 export const AuthService = {
